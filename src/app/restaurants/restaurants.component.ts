5b095c871d10bc9d82739bebec8ddf4bff5c7cf9
Marcelo Ratton <rattones@marceloratton.com>
2018-07-16 14:50:38 -0300

    aula - angular class service

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Restaurant } from './restaurant/restaurant.model';
+import { RestaurantsService } from './restaurants.service';
 
 @Component({
   selector: 'mt-restaurants',
@@ -8,36 +9,12 @@ import { Restaurant } from './restaurant/restaurant.model';
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[] = [
-    {
-      id: "bread-bakery",
-      name: "Bread & Bakery",
-      category: "Bakery",
-      deliveryEstimate: "25m",
-      rating: 4.9,
-      imagePath: "assets/img/restaurants/breadbakery.png"
-    },
-    {
-      id: "burger-house",
-      name: "Burger House",
-      category: "Hamburgers",
-      deliveryEstimate: "100m",
-      rating: 3.5,
-      imagePath: "assets/img/restaurants/burgerhouse.png"
-    },
-    {
-      id: "coffee-corner",
-      name: "Coffee Corner",
-      category: "Coffee Shop",
-      deliveryEstimate: "30-40m",
-      rating: 4.8,
-      imagePath: "assets/img/restaurants/coffeecorner.png"
-    }    
-  ]
+  restaurants: Restaurant[]
 
-  constructor() { }
+  constructor(private restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
+    this.restaurants = this.restaurantsService.restaurants()
   }
 
 }
