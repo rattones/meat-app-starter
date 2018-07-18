66fb0c83d4a6f2865a3e6b8760d81cf0407ee08c
Marcelo Ratton <rattones@marceloratton.com>
2018-07-18 17:08:19 -0300

    aula: menu item | currency

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -9,12 +9,12 @@ import { Restaurant } from "./restaurant/restaurant.model"
 
 import { MEAT_API } from "../app.api"
 import { ErrorHandler } from "../app.error-handler"
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
 
 @Injectable() 
 export class RestaurantsService {
 
     constructor(private http: Http) {}
-
 
     restaurants(): Observable<Restaurant[]> {
         // return this.rests
@@ -31,6 +31,12 @@ export class RestaurantsService {
 
     reviewsOfRestaurant(id: string):  Observable<any> {
         return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
